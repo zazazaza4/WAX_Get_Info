803bd3afea8d34e47690d0d3c2efa48a4556b56f
Yaroslav <6715414@gmail.com>
2023-03-26 13:32:02 +0300

    added const for requests

---
+++ consts.js
@@ -0,0 +1,6 @@
+const URL_WAX_MARKET = "https://wax.api.atomicassets.io";
+export const GET_USER = URL_WAX_MARKET + "/atomicassets/v1/accounts/";
+
+export const POST_CURRENCY_BALANCE =
+  "https://wax.greymass.com/v1/chain/get_currency_balance";
+export const GET_TOKENS = "https://wax.light-api.net/api/balances/wax/";
